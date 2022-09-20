af990d1a05455fb595ca4182dc4ef239acd49120
hanyuegai <cecilia759@163.com>
2022-09-20 14:15:44 +0800

    feat: create ShoppingCartViewController

---
+++ POS App 1.0/ShoppingCartViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingCartViewController.swift
+//  POS App 1.0
+//
+//  Created by Hanyue Gai on 2022/9/20.
+//
+
+import Foundation
+import UIKit
+
+class ShoppingCartViewController: UIViewController {
+    
+}
