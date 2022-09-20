8508ce3d3800ee7fd295356daed880102398aaf9
hanyuegai <cecilia759@163.com>
2022-09-20 10:50:45 +0800

    init: create git repo for POS App1.0

---
+++ POS App 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POS App 1.0
+//
+//  Created by Hanyue Gai on 2022/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
