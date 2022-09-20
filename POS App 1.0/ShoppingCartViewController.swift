8228c14a99be9f62188b6ea13b50523b03530296
hanyuegai <cecilia759@163.com>
2022-09-20 14:20:33 +0800

    feat: add IBOutlet for label, textField and button in shopping cart page

--- POS App 1.0/ShoppingCartViewController.swift
+++ POS App 1.0/ShoppingCartViewController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class ShoppingCartViewController: UIViewController {
     
+    @IBOutlet weak var shoppingCartLabel: UILabel!
+
+    @IBOutlet weak var totalPieceText: UITextField!
+    
+    @IBOutlet weak var totalAmoutText: UITextField!
+    
+    @IBOutlet weak var totalReductionText: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        
+    }
+    
 }
