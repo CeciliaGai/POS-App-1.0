78e9d6e2f22c69d9ede68ba7e1935baedcb4b644
hanyuegai <cecilia759@163.com>
2022-09-20 17:28:26 +0800

    feat: create CartItem

---
+++ POS App 1.0/CartItem.swift
@@ -0,0 +1,15 @@
+//
+//  CartItem.swift
+//  POS App 1.0
+//
+//  Created by Hanyue Gai on 2022/9/20.
+//
+
+import Foundation
+
+struct CartItem {
+    let name: String
+    let number: Int
+    let total: Float
+    let reduction: Float
+}
