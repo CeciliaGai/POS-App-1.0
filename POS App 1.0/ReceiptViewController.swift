40f3b672e42568a42f86a209b971da724358ff7b
hanyuegai <cecilia759@163.com>
2022-09-20 14:43:26 +0800

    feat: add segue between itemList page and shoppingCart page

---
+++ POS App 1.0/ReceiptViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ReceiptViewController.swift
+//  POS App 1.0
+//
+//  Created by Hanyue Gai on 2022/9/20.
+//
+
+import Foundation
+import UIKit
+class ReceiptViewController: UIViewController {
+    
+    @IBOutlet weak var receiptLabel: UILabel!
+    
+    @IBOutlet weak var totalAmountText: UITextField!
+    
+    @IBOutlet weak var totalReductionText: UITextField!
+    
+    override func viewDidLoad() {
+        
+    }
+}
