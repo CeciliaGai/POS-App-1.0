110e4adfe4a07e12ba870845baab679068b17309
hanyuegai <cecilia759@163.com>
2022-09-21 10:48:16 +0800

    feat: create receiptItem model

---
+++ POS App 1.0/Model/ReceiptItem.swift
@@ -0,0 +1,15 @@
+//
+//  ReceiptItem.swift
+//  POS App 1.0
+//
+//  Created by Hanyue Gai on 2022/9/21.
+//
+
+import Foundation
+
+struct ReceiptItem {
+    let name: String
+    let number: Int
+    let price: Float
+    let subtotal: Float
+}
