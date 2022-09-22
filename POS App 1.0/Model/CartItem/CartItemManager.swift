4f9308c18df02f928348e20546fe0010495f5906
hanyuegai <cecilia759@163.com>
2022-09-22 14:12:07 +0800

    fix: change logic of shopping cart

--- POS App 1.0/Model/CartItem/CartItemManager.swift
+++ POS App 1.0/Model/CartItem/CartItemManager.swift
@@ -9,8 +9,8 @@ import Foundation
 struct CartItemManager {
     var purchasedDB: [String: CartItem] =
     [
-        "可口可乐": CartItem(name: "可口可乐", number: 0, total: 2, reduction: 0, price: 3),
-        "雪碧": CartItem(name: "雪碧", number: 0, total: 0, reduction: 0, price: 3),
+        "可口可乐": CartItem(name: "可口可乐", number: 1, total: 0, reduction: 0, price: 3),
+        "雪碧": CartItem(name: "雪碧", number: 1, total: 0, reduction: 0, price: 3),
         "苹果": CartItem(name: "苹果", number: 0, total: 0, reduction: 0, price: 5.5),
         "荔枝": CartItem(name: "荔枝", number: 0, total: 0, reduction: 0, price: 15),
         "电池": CartItem(name: "电池", number: 0, total: 0, reduction: 0, price: 2),
@@ -24,7 +24,7 @@ struct CartItemManager {
         for (itemName, cartItem) in purchasedDB {
             var cartItem = CartItem(name: "", number: 0, total: 0, reduction: 0, price: 0)
             cartItem = purchasedDB[itemName]!
-            if cartItem.total != 0 {
+            if cartItem.number != 0 {
                 purchasedItems.append(cartItem)
             }
         }
