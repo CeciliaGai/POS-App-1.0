8df705a8977350093d919cff21ad3ee9b0c1926f
hanyuegai <cecilia759@163.com>
2022-09-26 10:43:36 +0800

    feat: show reduction for single item

--- POS App 1.0/Model/Calculator.swift
+++ POS App 1.0/Model/Calculator.swift
@@ -15,6 +15,7 @@ struct Calculator {
     mutating func calculatingTotal() -> Double {
         let allItems = PurchasedDB.sharedDB
         var total = 0.0
+        var reduction = 0.0
         for (itemName, _) in allItems.itemsDB {
             var cartItem = CartItem(name: "", number: 0, total: 0, reduction: 0, price: 0, barcode: "")
             cartItem = allItems.itemsDB[itemName]!
@@ -22,10 +23,14 @@ struct Calculator {
                 if promotionList.items.contains(allItems.itemsDB[itemName]!.barcode) {
                     if ((allItems.itemsDB[itemName]!.number % 2) != 0) {
                         allItems.itemsDB[itemName]?.total = Double((cartItem.number + 1)/2) * cartItem.price
+                        allItems.itemsDB[itemName]?.reduction = Double(cartItem.number) * cartItem.price - Double((cartItem.number + 1)/2) * cartItem.price
                         total += allItems.itemsDB[itemName]!.total
+                        reduction += allItems.itemsDB[itemName]!.reduction
                     } else {
                         allItems.itemsDB[itemName]?.total = Double(cartItem.number/2) * cartItem.price
+                        allItems.itemsDB[itemName]?.reduction = Double(cartItem.number) * cartItem.price - Double(cartItem.number/2) * cartItem.price
                         total += allItems.itemsDB[itemName]!.total
+                        reduction += allItems.itemsDB[itemName]!.reduction
                     }
                 } else {
                     allItems.itemsDB[itemName]?.total = Double(cartItem.number) * cartItem.price
